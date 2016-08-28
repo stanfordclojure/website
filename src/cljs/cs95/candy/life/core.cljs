cb080e4e751da9be7ecdb8162e4800cf970d0bad
Allan Jiang <origami789@gmail.com>
2016-08-28 02:01:50 -0700

    beautiful introspection with slurp-dep

--- src/cljs/cs95/candy/life/core.cljs
+++ src/cljs/cs95/candy/life/core.cljs
@@ -3,10 +3,12 @@
 
 ;; http://clj-me.cgrand.net/2011/08/19/conways-game-of-life/
 
+;; compute neighbors
 (defn neighbors [[x y]]
   (for [dx [-1 0 1] dy (if (zero? dx) [-1 1] [-1 0 1])]
     [(+ dx x) (+ dy y)]))
 
+;; compute cells available in next generation
 (defn step [cells]
   (set (for [[loc n] (frequencies (mapcat neighbors cells))
              :when (or (= n 3) (and (= n 2) (cells loc)))]
--- src/cljs/cs95/views/candy.cljs
+++ src/cljs/cs95/views/candy.cljs
@@ -5,15 +5,13 @@
             [cs95.candy.life.core :as life]
             [cs95.components.bootstrap :as bs]
             [cljs.core.async :as async :refer [timeout <!]])
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]
+                   [cs95.utils.helper :refer [slurp-dep]]))
 
 (defonce state (r/atom #{[-1 0] [0 0] [1 0]}))
 
 (defn evolve [state]
   (swap! state life/step))
-
-(defn view []
-  [life-view/grid state])
 
 (defn play []
   (go-loop []
@@ -23,3 +21,23 @@
 
 (defn interesting-seed []
   (reset! state #{[-1 0] [-1 1] [0 -1] [0 0] [1 0]}))
+
+(defn explanation []
+  [:div
+   [:p "This core of this game of life implementation occurs in just 7 lines of code."]
+   [:h3 "cs95.candy.life.core"]
+   [:pre (slurp-dep "src/cljs/cs95/candy/life/core.cljs")]
+   [:h3 "cs95.candy.life.view"]
+   [:pre (slurp-dep "src/cljs/cs95/candy/life/view.cljs")]
+   [:h3 "cs95.views.candy"]
+   [:pre (slurp-dep "src/cljs/cs95/views/candy.cljs")]])
+
+(defn view []
+  [:div
+   [life-view/grid state]
+   [:br]
+   [bs/Button {:on-click play} "Start Simulation"]
+   [bs/Button {:on-click interesting-seed} "Interesting..."]
+   [:hr]
+   [explanation]])
+
