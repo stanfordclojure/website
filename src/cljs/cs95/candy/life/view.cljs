abf4fe723331bce325074b3996f1873432376c26
Allan Jiang <origami789@gmail.com>
2016-08-28 01:49:06 -0700

    follow tutorial, use svg as view

--- src/cljs/cs95/candy/life/view.cljs
+++ src/cljs/cs95/candy/life/view.cljs
@@ -2,17 +2,14 @@
   (:require [reagent.core :as reagent])
   (:require-macros [cs95.utils.re-frame :refer [with-subs]]))
 
-(defn cell [size coords]
-  (let [size (or size 50)]
-    [:td {:style {:border "1px solid black"
-                  :width size
-                  :height size}}])
-  #_(with-subs [state [:life/cell-state coords]]
-      ))
+;; https://www.youtube.com/watch?v=dxLRQ0aKTOQ
 
-(defn board [x y width height]
-  [:table
-   [:tbody
-    [:tr [cell] [cell] [cell]]
-    [:tr [cell] [cell] [cell]]
-    [:tr [cell] [cell] [cell]]]])
+(defn grid [state]
+  [:svg {:width 500
+         :height 500
+         :viewBox "-250 -250 500 500"}
+   (for [[x y] @state]
+     [:rect {:width 20
+             :height 20
+             :x (* x 20)
+             :y (* y 20)}])])
--- src/cljs/cs95/views/candy.cljs
+++ src/cljs/cs95/views/candy.cljs
@@ -1,10 +1,25 @@
 (ns cs95.views.candy
   (:require [re-frame.core :as re-frame]
-            [cs95.candy.life.view :as life]
-            [cs95.components.bootstrap :as bs]))
+            [reagent.core :as r]
+            [cs95.candy.life.view :as life-view]
+            [cs95.candy.life.core :as life]
+            [cs95.components.bootstrap :as bs]
+            [cljs.core.async :as async :refer [timeout <!]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+
+(defonce state (r/atom #{[-1 0] [0 0] [1 0]}))
+
+(defn evolve [state]
+  (swap! state life/step))
 
 (defn view []
-  [:div
-   "CANDY"
-   [life/board]])
+  [life-view/grid state])
 
+(defn play []
+  (go-loop []
+    (<! (timeout 100))
+    (swap! state life/step)
+    (recur)))
+
+(defn interesting-seed []
+  (reset! state #{[-1 0] [-1 1] [0 -1] [0 0] [1 0]}))
